2221a956c249ac30e64878a9865e4d22a2871fa9
Yifan Lin <92171160+linyifan1024@users.noreply.github.com>
2023-04-18 00:10:08 -0500

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,32 @@
 import express from "express";
+let articleInfo = [
+  {
+    name: "learn-react",
+    upvotes: 0,
+  },
+  {
+    name: "learn-node",
+    upvotes: 0,
+  },
+  {
+    name: "mongodb",
+    upvotes: 0,
+  },
+];
 
 const app = express();
 app.use(express.json());
-app.post("/hello", (req, res) => {
-  console.log(req.body);
-  res.send(`Hello ${req.body.name}!`);
-});
 
+app.put("/api/articles/:name/upvote", (req, res) => {
+  const { name } = req.params;
+  const article = articleInfo.find((article) => article.name === name);
+  if (article) {
+    article.upvotes += 1;
+    res.send(
+      `The article ${name} has been upvoted for ${article.upvotes} times`
+    );
+  } else {
+    res.send(`The article ${name} does not exist`);
+  }
+});
 app.listen(8000, () => console.log("Listening on port 8000"));
