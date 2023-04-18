fcb58dec314488ab4030c34c60188caacca91c04
Yifan Lin <92171160+linyifan1024@users.noreply.github.com>
2023-04-17 23:53:41 -0500

    set up back end

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.post("/hello", (req, res) => {
+  console.log(req.body);
+  res.send(`Hello ${req.body.name}!`);
+});
+
+app.listen(8000, () => console.log("Listening on port 8000"));
